50c13a5c814b1cc6b0d07f133ee688ac5c86ea7d
Chris Kennedy <chrisken@gmail.com>
2015-06-23 10:02:50 -0700

    Copy lower triangle to upper triangle of the probability matrix.

--- R/htEstimate.R
+++ R/htEstimate.R
@@ -21,6 +21,9 @@ createProbMatrix = function(assignments, byrow = F) {
   k = length(assign_levels)
   n = nrow(assignments)
 
+  #TODO: throw an error if k == 1
+  #TODO: throw an error if n == 1
+
   # Matrix to store the results.
   result = matrix(nrow = n*k, ncol=n*k)
   #cat("Result dimensions:", n*k, "by", n*k, "\n")
@@ -29,22 +32,49 @@ createProbMatrix = function(assignments, byrow = F) {
   # We start at the top-left diagonal and work down the column.
   for (col in 1:k) {
     for (row in col:k) {
-      # Starting row & col in the matrix.
-      start_row = 1 + (row - 1)  * n
-      end_row = start_row + n - 1
-      start_col = 1 + (col - 1) * n
-      end_col = start_col + n - 1
+      dest = getRawMatrixEntries(row, col, n)
       #cat("Start row:", start_row, "End row:", end_row, "Start col:", start_col, "End col:", end_col, "\n")
       probs = generateAssignmentProbs(row, col, assignments, assign_levels)
       #print(probs)
-      result[start_row:end_row, start_col:end_col] = probs
+      result[dest$start_row:dest$end_row, dest$start_col:dest$end_col] = probs
     }
   }
 
+  #cat("Current matrix after creating lower triangle:\n")
+  #print(result)
+
   # Copy lower triangle to the upper triangle.
+
+  for (col in 2:k) {
+    for (row in seq(1, col-1)) {
+
+      # Target is the matrix in the lower triangle that we're copying from.
+      target = getRawMatrixEntries(col, row, n)
+      #cat("Copying target:\n")
+      #with(target, cat("Row:", col, "Col:", row, "Start row:", start_row, "End row:", end_row, "Start col:", start_col, "End col:", end_col, "\n"))
+
+      targetMat = result[target$start_row:target$end_row, target$start_col:target$end_col]
+      #print(targetMat)
+
+      # Destination is the matrix in the upper triangle.
+      dest = getRawMatrixEntries(row, col, n)
+
+      # TODO: determine if we should be transposing the targetMat here.
+      result[dest$start_row:dest$end_row, dest$start_col:dest$end_col] = targetMat
+    }
+  }
 
   # Return the result.
   return(result)
+}
+
+getRawMatrixEntries = function(row, col, n) {
+  # Starting row & col in the matrix.
+  start_row = 1 + (row - 1)  * n
+  end_row = start_row + n - 1
+  start_col = 1 + (col - 1) * n
+  end_col = start_col + n - 1
+  return(list(start_row=start_row, end_row=end_row, start_col=start_col, end_col=end_col))
 }
 
 generateAssignmentProbs = function(row, col, assignments, assign_levels) {
